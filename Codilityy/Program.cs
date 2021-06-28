d5a2108856dac073c1fde8a11e9a14a29dc409eb
Ifeanyi Odom <ifeanyi.odom@axamansard.com>
2021-06-28 12:26:22 +0100

    First Commit

---
+++ Codilityy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace Codilityy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sorting sorting = new Sorting();
+
+            int[] array = new int[8] { 30, 3, 14, 5, 6, 8, 2, 71 };
+
+            sorting.QuickSort(array, 0, array.Length - 1);
+         
+            foreach(var i in array)
+            {
+                Console.WriteLine($"\n\n{i},");
+            }
+            //double num = Console.Read();
+            //Console.WriteLine(fact.GetFact(num));
+            Console.ReadLine();
+        }
+      
+
+    }
+
+    public class Factorial
+    {
+        StringBuilder sb = new StringBuilder();
+        public double GetFact(double num)
+        {
+            sb.Append($"{num}! ");
+            if (num == 1 || num == 0)
+            {
+                Console.WriteLine(sb);
+                return num == 1 ? 1 : 0;
+            }
+            return num * GetFact(num - 1);
+        }
+    }
+}
---
+++ Codilityy/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace Codilityy
+{
+    public class Solution
+        {
+            public string solution(string S)
+            {
+                // write your code in C# 6.0 with .NET 4.5 (Mono)
+                S = removeNonDigits(S);
+                return phoneNumberFormat(S, numberSize(S));
+            }
+
+            private bool numberSize(string phoneNumber)
+            {
+                return phoneNumber.Length % 3 == 1;
+            }
+
+            private string removeNonDigits(string digits)
+            {
+                return digits.Replace("[^0-9]", "");
+            }
+            private string phoneNumberFormat(string digit, bool lastInstance)
+            {
+                string tempNum = "";
+                int dashCounter = 0;
+                for (int i = 0; i < digit.Length; i++)
+                {
+                    if (dashCounter < 3)
+                    {
+                        var conCatSubString = digit[i];
+                        tempNum = tempNum + conCatSubString;
+                        dashCounter++;
+                    }
+                    else if (dashCounter == 3)
+                    {
+                        tempNum = tempNum + "-";
+                        var conCatSubString2 = digit[i];
+                        tempNum = tempNum + conCatSubString2;
+                        dashCounter = 1;
+                    }
+                }
+
+                if (lastInstance)
+                {
+                    char[] _temp = tempNum.ToCharArray();
+                    _temp[_temp.Length - 2] = _temp[_temp.Length - 3];
+                    _temp[_temp.Length - 3] = '-';
+                    tempNum = new string(_temp);
+                }
+                return tempNum;
+            }
+        }
+
+    }
