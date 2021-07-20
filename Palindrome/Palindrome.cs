486ea8f781f2025e0f779ffdddb55139c3e59909
Ifeanyi Odom <iodom@appzonegroup.com>
2021-07-20 15:30:50 +0100

    Commit dev-Palindrome

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Recursion
+{
+    public class Palindrome
+    {
+        private int lengthTracker;
+        private string inputText;
+        public Palindrome(string input)
+        {
+            lengthTracker = input.Length;
+            inputText = input;
+        }
+
+        public bool IsPalindrome(int length)
+        {
+            //base case
+            if (length == 0 || length == 1)
+                return true;
+
+            //smallest amount of work to be performed by the recursive function
+
+            if (inputText[0] == inputText[length - 1])
+            {
+                lengthTracker = inputText.Substring(1, length-1).Length;//kayak
+                inputText = inputText.Substring(1, lengthTracker-1);
+                return IsPalindrome(lengthTracker -1);
+            }
+            return false;
+        }
+    }
+}
