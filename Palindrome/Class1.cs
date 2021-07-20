0485a356fe7992e3bee812c618afe78b67e1d96b
Ifeanyi Odom <iodom@appzonegroup.com>
2021-07-20 10:46:01 +0100

    Commit All

---
+++ Palindrome/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palindrome
+{
+    public class Class1
+    {
+        //public bool IsPalindrome(string input)
+        //{
+        //    if(input[0] == "")
+        //}
+    }
+}
