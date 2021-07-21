fcb927235ca1a3e1aebc23329172c0244a4ba330
Ifeanyi Odom <iodom@appzonegroup.com>
2021-07-21 03:53:25 +0100

    Commit All

---
+++ Algorithms.Test/PalindromeTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Recursion;
+
+namespace Algorithms.Test
+{
+    public class PalindromeTest
+    {
+        [Theory]
+        [InlineData("sasdrdsas", true)]
+        public void IsPalindrome(string word, bool expectedResult)
+        {
+            Palindrome palindrome = new Palindrome(word);
+            var isPalindrome = palindrome.IsPalindrome(word.Length);
+
+            Assert.Equal(expectedResult, isPalindrome);
+
+        }
+    }
+}
---
+++ Algorithms.Test/WordReversalTest.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using StackImplementation;
+
+namespace Algorithms.Test
+{
+    public class WordReversalTest
+    {
+        [Fact]
+        public void WordRevers()
+        {
+            var word = "abcdefghijklmnpqrstuvwxyz";
+            WordReversal reverse = new WordReversal(word);
+
+            var reversedWord = reverse.reverseString(word);
+
+            Assert.Equal("zyxwvutsrqpnmlkjihgfedcba", reversedWord);
+
+        }
+    }
+}
