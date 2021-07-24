be30ce64db0c3d2d92a5da2a06358e242bcfb5e5
Ifeanyi Odom <iodom@appzonegroup.com>
2021-07-24 14:51:40 +0100

    Commit Missing Integer

---
+++ Codilityy/MissingInteger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codilityy
+{
+    public class MissingInteger
+    {
+        public int GetMissingInteger(int[] A)
+        {
+            HashSet<int> foundNumber = new HashSet<int>();
+            for(int i=0; i<A.Length; i++)
+            {
+                if(A[i] > 0)
+                {
+                    foundNumber.Add(A[i]);
+                }
+            }
+
+            int result = 1;
+            while (foundNumber.Contains(result))
+            {
+                result++;
+            }
+            return result;
+        }
+    }
+
+}
