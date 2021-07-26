552d74dda2df780001e02b515d55d820d2410754
Ifeanyi Odom <iodom@appzonegroup.com>
2021-07-26 08:16:48 +0100

    Commit Addition of (n) Numbers

---
+++ Codilityy/AdditionOfNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace Codilityy
+{
+    public class AdditionOfNumbers
+    {
+        public int AddTotalNumbers(int n)//Approach 1
+        {
+            int result = 0;
+            result = (n * (n + 1))/2;
+            return result;
+        }
+        //1+2+3+4+5
+        public int AddTotalNumbers2(int n)
+        {
+            int result = 0;
+            for(int i=0; i<=n; i++)
+            {
+                result += i; 
+            }
+            return result;
+        }
+    }
+}
