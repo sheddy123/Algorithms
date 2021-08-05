e133b4f945323c03ada8dc6ff81261b1a15146ff
Ifeanyi Odom <iodom@appzonegroup.com>
2021-08-05 04:09:46 +0100

    Commit All

--- Algorithms.Test/ThreeSumClosestTest.cs
+++ Algorithms.Test/ThreeSumClosestTest.cs
@@ -9,14 +9,14 @@ namespace Algorithms.Test
     {
         SumClosest sumClosest = new SumClosest();
 
-        [Theory]
-        [InlineData(new int[4] { 0, 2, 1,-3 }, 0)]
-        public void Three_SumClosest(int[] values, int expectedValues)
-        {
-            var _sumClosest = sumClosest.ThreeSumClosest1(values, 1);
+        //[Theory]
+        //[InlineData(new int[4] { 0, 2, 1,-3 }, 0)]
+        //public void Three_SumClosest(int[] values, int expectedValues)
+        //{
+        //    var _sumClosest = sumClosest.ThreeSumClosest1(values, 1);
             
-            Assert.Equal(expectedValues, _sumClosest);
+        //    Assert.Equal(expectedValues, _sumClosest);
 
-        }
+        //}
     }
 }
