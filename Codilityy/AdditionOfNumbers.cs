f631d4aa452c527c5f0681023856caf506f29ee2
Ifeanyi Odom <iodom@appzonegroup.com>
2021-08-07 11:48:13 +0100

    Commit All

--- Codilityy/AdditionOfNumbers.cs
+++ Codilityy/AdditionOfNumbers.cs
@@ -1,4 +1,8 @@
-﻿namespace Codilityy
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Codilityy
 {
     public class AdditionOfNumbers
     {
@@ -11,6 +15,9 @@
         //1+2+3+4+5
         public int AddTotalNumbers2(int n)
         {
+            List<int> largestNumber = new List<int>();
+            Regex.Matches("", ""); 
+            largestNumber.Max();
             int result = 0;
             for(int i=0; i<=n; i++)
             {
--- Codilityy/Program.cs
+++ Codilityy/Program.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
 using LeetCode;
+using System.Text.RegularExpressions;
 
 namespace Codilityy
 {
@@ -15,6 +16,9 @@ namespace Codilityy
     {
         static void Main(string[] args)
         {
+            Factorial fac = new Factorial();
+            var word = "Forget  CVs..Save time . x x";
+            var count = fac.solution(word);
             int[] duplicates = new int[3] { 1, 1, 2 };
             RemoveDuplicatesSortedArray array = new RemoveDuplicatesSortedArray();
             var output = array.ReoveDuplicates(duplicates);
@@ -30,8 +34,26 @@ namespace Codilityy
 
        
     }
+
     public class Factorial
     {
+        //public int solution(string S)
+        //{
+        //    // write your code in C# 6.0 with .NET 4.5 (Mono)
+        //    List<int> largestNumber = new List<int>();
+        //    char[] charsToTrim = { '.', ',', '?', '!' };
+            
+
+        //    foreach (var word in S.TrimEnd(charsToTrim).Split(charsToTrim, StringSplitOptions.RemoveEmptyEntries))
+        //    {
+        //        string newWprd = Regex.Replace(word, @"\s+", " ");
+        //        largestNumber.Add(newWprd.ToUpper().Trim().Split(' ').Count());
+        //    }
+            
+
+        //    return largestNumber.Max();
+        //}
+
         StringBuilder sb = new StringBuilder();
         
         public double GetFact(double num)
