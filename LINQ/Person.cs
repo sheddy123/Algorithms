7db4aa4656ee27f353273f28a97e44207716e309
Ifeanyi Odom <iodom@appzonegroup.com>
2021-08-14 04:30:22 +0100

    Commit All

---
+++ LINQ/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LINQ
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} | {FirstName} {LastName} | {City} | {BirthDate.ToShortDateString()} | {Salary}";
+            
+        }
+    }
+}
