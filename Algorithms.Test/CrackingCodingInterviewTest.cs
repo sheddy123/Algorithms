f75bf8813ae6bbe8c614e3ff7b105425bc4eb186
Ifeanyi Odom <iodom@appzonegroup.com>
2021-08-25 04:33:17 +0100

    Commit All

---
+++ Algorithms.Test/CrackingCodingInterviewTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using CrackingCodingInterview;
+
+namespace Algorithms.Test
+{
+    public class CrackingCodingInterviewTest
+    {
+        StringArrays stringTest = new StringArrays();
+
+        [Theory]
+        [InlineData("abcdefghijklmnopqrstuvwxhhjyz")]
+        public void IsUnique(string stringToTest)
+        {
+            var result = stringTest.IsUnique(stringToTest);
+
+            Assert.False(result);
+        }
+    }
+}
