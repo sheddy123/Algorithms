ea2ddff2a7cecb89fd818cf0ee468a331950e67a
Ifeanyi Odom <iodom@appzonegroup.com>
2021-10-18 07:18:03 +0100

    Commit staged

---
+++ Algorithms.Test/MostCommonWordTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using LeetCode;
+using System;
+namespace Algorithms.Test
+{
+    public class MostCommonWordTest
+    {
+        [Theory]
+        [InlineData("a.", new string[] { })]
+        public void MostCommonTest(string paragraph, string[] banned)
+        {
+            //Arrange
+            MostCommon mostCommon = new MostCommon();
+
+            //Act
+            var result = mostCommon.MostCommonWord(paragraph, banned);
+
+            //Assert
+            Assert.Equal("a", result);
+        }
+    }
+}
