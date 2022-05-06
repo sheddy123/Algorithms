e9cbe08ea6581c00b504c92663354b879b49a06c
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2022-05-06 03:15:50 +0100

    Implemented a basic Recursion and also added some tests

---
+++ Algorithms.Test/AlgoExpectRecursionTest.cs
@@ -0,0 +1,21 @@
+﻿using StackImplementation;
+using Xunit;
+
+namespace Algorithms.Test
+{
+    public class AlgoExpectRecursionTest
+    {
+        [Fact]
+        public void GetFactorialTest()
+        {
+            //Act
+           // AlgoExpertSearch fact = new AlgoExpertSearch();
+
+            //Arrange
+            var result = AlgoExpertRecursion.Factorial(5);
+
+            //Assert
+            Assert.Equal(120, result);
+        }
+    }
+}
